d8fbdbcd244cebb246b8d59cb59373b87a92cd50
Avrilkamu2727 <agneskamu31@gmail.com>
2024-03-07 20:55:17 +0800

    MoneyTracker

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import SplashScreen from './src/pages/SplashScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 const App =() => {
-  return <SplashScreen/>;
+  return{
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen 
+  name="SplashScreen" component={SplashScreen} options{}/>
+  </Stack.Navigator>
+  <SplashScreen/>
+  </NavigationContainer>
+  };
 };
 export default App;
